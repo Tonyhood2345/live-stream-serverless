77fb519cfaec94c00a63af49ea943caa2d3de40e
Tonyhood2345 <33382873+Tonyhood2345@users.noreply.github.com>
2026-09-23 18:52:40 +0200

    Sincronizza modulo scripts/story_publisher/graphics/templates/__init__.py

---
+++ scripts/story_publisher/graphics/templates/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Package Templates Grafici per Storie e Flyer"""
+from .splitscreen import crea_grafica_flyer_split_screen, crea_story_splitscreen_9_16
+from .luxury_glass import crea_grafica_luxury_glass, crea_story_luxury_glass_9_16
+from .editorial import crea_grafica_editorial, crea_story_editorial_9_16
+
+__all__ = [
+    "crea_grafica_flyer_split_screen",
+    "crea_story_splitscreen_9_16",
+    "crea_grafica_luxury_glass",
+    "crea_story_luxury_glass_9_16",
+    "crea_grafica_editorial",
+    "crea_story_editorial_9_16"
+]

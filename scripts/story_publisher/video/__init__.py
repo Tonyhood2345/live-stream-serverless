c22b094f3c1d8d28af26ba03e71d281a92b9e703
Tonyhood2345 <33382873+Tonyhood2345@users.noreply.github.com>
2026-09-23 18:53:17 +0200

    Sincronizza modulo scripts/story_publisher/video/__init__.py

---
+++ scripts/story_publisher/video/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Package Video: Motore di Compositing e Rendering Video FFmpeg"""
+from .video_engine import genera_video_da_clip_o_foto
+
+__all__ = ["genera_video_da_clip_o_foto"]
